e700a191b65b805947100f37dc297774f7b18a51
Ted Sazon <tedsazon@gmail.com>
2019-01-04 02:40:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,1 @@
-class Artist
-  attr_accessor :name, :songs
 
-  def initialize(name)
-    @name = name
-    @songs = []
-  end
-
-  def add_song(song)
-    @songs << song
-  end
-
-  def songs
-    @songs
-  end
-end
