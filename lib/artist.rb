5e15396d2a0980a093c861b1c1e67111b269108e
Ted Sazon <tedsazon@gmail.com>
2019-01-04 02:40:51 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,16 @@
+class Artist
+  attr_accessor :name, :songs
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def songs
+    @songs
+  end
+end
