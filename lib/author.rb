07c5eb5eff6e9fb4dc0a32c12dee39a73b949fee
Ted Sazon <tedsazon@gmail.com>
2019-01-04 03:16:16 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -3,7 +3,7 @@ class Author
   attr_accessor :name, :posts
 
   @@post_count = 0
-  
+
   def initialize(name)
     @name = name
     @posts = []
@@ -12,7 +12,7 @@ class Author
   def add_post(post)
     @posts << post
     post.author = self
-
+    @@post_count += 1
   end
 
   def add_post_by_title(title)
